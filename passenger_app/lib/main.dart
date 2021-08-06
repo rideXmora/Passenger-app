669bb82a1226d9472f84369ce09068956319ef49
Avishka <mailavishka@gmail.com>
2021-08-06 13:08:08 +0530

    changing app name

--- passenger_app/lib/main.dart
+++ passenger_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RideX Passenger APP',
+      title: 'RideX',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
