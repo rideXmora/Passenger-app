e22559a4e844ff732c6db426e97ac9d4e07e6c96
Avishka <mailavishka@gmail.com>
2021-10-22 09:52:55 +0530

    changing ride states

---
+++ passenger_app/lib/utils/ride_request_state_enum.dart
@@ -0,0 +1,16 @@
+// enum RideState {
+//   RIDERREQUEST,
+//   ACCEPTED,
+//   ONTRIP,
+//   TRIPCOMPLETED,
+//   RATEANDCOMMENT,
+//   NOTRIP,
+// }
+
+enum RideRequestState {
+  SELECTVECHICLE,
+  PENDING,
+  ACCEPTED,
+  TIMEOUT,
+  NOTRIP,
+}
